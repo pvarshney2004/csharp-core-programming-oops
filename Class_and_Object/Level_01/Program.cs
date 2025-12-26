577b9fb937d2cb8d902f3cfe4e4d15f16961dde4
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-26 16:32:35 +0530

    [Prashant] add Program.cs file

---
+++ Class_and_Object/Level_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Core_Programming_OOPs.Class_and_Object.Level_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Employee class
+            //Employee emp = new Employee("John Doe", 101, 54000);
+            //emp.DisplayEmployeeDetails();
+
+            // Create an instance of the Circle class
+            //Circle circle = new Circle(5.0);
+            //double area = circle.CalculateArea();
+            //double circumference = circle.CalculateCircumference();
+            //Console.WriteLine($"Circle with Radius: {circle.Radius} having Area: {area:F2} and Circumference: {circumference:F2}");
+
+            // Create an instance of the Book class
+            Book book = new Book("C# Programming", "Jon Skeet", 599);
+            book.DisplayBookDetails();
+
+        }
+    }
+
+}
