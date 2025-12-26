98db165027f8b5d2414288ffaeedb11aa683d2ec
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-26 16:31:58 +0530

    [Prashant] add program to implement Circle class and find area and circumference

---
+++ Class_and_Object/Level_01/Circle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Class_and_Object.Level_01
+{
+    internal class Circle
+    {
+        public double Radius;
+        // constructor to initialize circle radius
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        // methods to calculate area and circumference of the circle
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
