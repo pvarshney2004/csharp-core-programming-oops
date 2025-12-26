92a9f1ee58235f7fd49efa9fe3a2e473748ffe33
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-26 16:31:12 +0530

    [Prashant] add program to implement Employee class and displaying Employee info

---
+++ Class_and_Object/Level_01/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Class_and_Object.Level_01
+{
+    internal class Employee
+    {
+        public string EmployeeName;
+        public int EmployeeID;
+        public int EmployeeSalary;
+
+        // Constructor to initialize employee details
+        public Employee(string name, int id, int salary)
+        {
+            EmployeeName = name;
+            EmployeeID = id;
+            EmployeeSalary = salary;
+        }
+
+        // Method to display employee details
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine("Employee Name: " + EmployeeName);
+            Console.WriteLine("Employee ID: " + EmployeeID);
+            Console.WriteLine("Employee Salary: " + EmployeeSalary);
+        }
+
+    }
+}
