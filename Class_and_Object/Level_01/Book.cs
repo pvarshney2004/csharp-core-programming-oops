b13143b04d6d8e87141c213b9d9a207413ac1f13
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-26 16:29:07 +0530

    [Prashant] add program to implement Book class and displaying book info

---
+++ Class_and_Object/Level_01/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Class_and_Object.Level_01
+{
+    internal class Book
+    {
+        public string BookTitle;
+        public string BookAuthor;
+        public int BookPrice;
+
+        // Constructor to initialize book details
+        public Book(string title, string author, int price)
+        {
+            BookTitle = title;
+            BookAuthor = author;
+            BookPrice = price;
+        }
+        // Method to display book details
+        public void DisplayBookDetails()
+        {
+            Console.WriteLine("Book Title: " + BookTitle);
+            Console.WriteLine("Book Author: " + BookAuthor);
+            Console.WriteLine("Book Price: " + BookPrice);
+        }
+
+    }
+}
