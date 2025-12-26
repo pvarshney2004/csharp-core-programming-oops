c12dd47128746d839c97d8fd0589c094c06f15ab
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-26 22:00:43 +0530

    [Prashant] add program to implement Book class with default and parameterized constructors

---
+++ Constructors/Level_01/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Constructors.Level_01
+{
+    internal class Book
+    {
+        /*
+Book Class
+o Create a Book class with attributes title, author, and price.
+o Provide both default and parameterized constructors.
+         */
+        public string Title;
+        public string Author;
+        public double Price;
+        // Default Constructor
+        public Book()
+        {
+            Title = "Default Title";
+            Author = "Default Author";
+            Price = 0.0;
+        }
+        // Parameterized Constructor
+        public Book(string title, string author, double price)
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+        }
+    }
+}
