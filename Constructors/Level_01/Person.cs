4e88fb55450eaf3bd70f8b158312fe0123f5fa47
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-29 11:25:07 +0530

    [Prashant] add program to implement Person class with parameterized and copy constructors

---
+++ Constructors/Level_01/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Constructors.Level_01
+{
+    internal class Person
+    {
+        /*
+Person Class (Copy Constructor)
+o Create a Person class with a copy constructor that clones another person's
+attributes.
+         */
+        public string Name;
+        public int Age;
+        // Parameterized Constructor
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        // Copy Constructor
+        public Person(Person other)
+        {
+            Name = other.Name;
+            Age = other.Age;
+        }
+    }
+}
