cf476255405222f60df3c5429fafaf10be879833
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-29 11:31:34 +0530

    [Prashant] add program to implement Product class to demonstrate instance and class members

---
+++ Constructors/Level_01/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Constructors.Level_01
+{
+    internal class Product
+    {
+        /*
+Problem : Product Inventory
+● Create a Product class with:
+o Instance Variables: productName, price.
+o Class Variable: totalProducts (shared among all products).
+● Implement the following methods:
+o An instance method DisplayProductDetails() to display the details of a
+product.
+o A class method DisplayTotalProducts() to show the total number of
+products created.
+         */
+        public string ProductName;
+        public double Price;
+        public static int TotalProducts = 0;
+        // parameterized Constructor
+        public Product(string productName, double price)
+        {
+            ProductName = productName;
+            Price = price;
+            TotalProducts++;
+        }
+        // instance method to display product details
+        public void DisplayProductDetails()
+        {
+            Console.WriteLine($"Product Name: {ProductName}, Price: {Price}");
+        }
+        // class method to display total products
+        public static void DisplayTotalProducts()
+        {
+            Console.WriteLine($"Total Products: {TotalProducts}");
+        }
+
+    }
+}
