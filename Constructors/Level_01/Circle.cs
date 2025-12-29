bb3777df8588225144a014597b6829b21e73ed6f
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-29 11:23:36 +0530

    [Prashant] add program to implement Circle class with default and parameterized constructors

---
+++ Constructors/Level_01/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Constructors.Level_01
+{
+    internal class Circle
+    {
+        /*
+Circle Class
+o Write a Circle class with a radius attribute.
+o Use constructor chaining to initialize radius with both default and
+user-provided values.
+         */
+        public double Radius;
+        // Default Constructor
+        public Circle() : this(1.0)
+        {
+        }
+        // Parameterized Constructor
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+    }
+}
