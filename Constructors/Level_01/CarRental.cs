9ee8db9f93a3f9d0d7da1c9bba100c2673fad2ce
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-29 11:29:47 +0530

    [Prashant] add program to implement carRental class with default and parameterized constructors and method to calculate rental cost

---
+++ Constructors/Level_01/CarRental.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Constructors.Level_01
+{
+    internal class CarRental
+    {
+        /*
+Car Rental System
+o Create a CarRental class with attributes customerName, carModel, and
+rentalDays.
+o Add constructors to initialize the rental details and calculate total cost.
+         */
+        public string CustomerName;
+        public string CarModel;
+        public int RentalDays;
+        // assuming a fixed rate per day for simplicity
+        private double RatePerDay = 50.0;
+
+        // parameterized Constructor
+        public CarRental(string customerName, string carModel, int rentalDays)
+        {
+            CustomerName = customerName;
+            CarModel = carModel;
+            RentalDays = rentalDays;
+        }
+        // method to calculate total cost
+        public double CalculateTotalCost()
+        {
+            return RentalDays * RatePerDay;
+        }
+    }
+}
