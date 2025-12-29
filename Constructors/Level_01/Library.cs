ea4d4a429805b4babc081b1c2c0ee4d16e5127b0
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-12-29 11:29:10 +0530

    [Prashant] add program to implement Library class with default and parameterized constructors and method to borrow book

---
+++ Constructors/Level_01/Library.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core_Programming_OOPs.Constructors.Level_01
+{
+    internal class Library
+    {
+        /*
+Library Book System
+o Create a Book class with attributes title, author, price, and availability.
+o Implement a method BorrowBook() to borrow a book.
+         */
+        public string Title;
+        public string Author;
+        public double Price;
+        public bool IsAvailable;
+        // parameterized Constructor
+        public Library(string title, string author, double price)
+        {
+            Title = title;
+            Author = author;
+            Price = price;
+            IsAvailable = true; // true indicates books are available by default
+        }
+        // method to borrow a book
+        public bool BorrowBook()
+        {
+            if (IsAvailable)
+            {
+                IsAvailable = false;
+                return true; // borrowed
+            }
+            else
+            {
+                return false; // book not available
+            }
+        }
+    }
+}
